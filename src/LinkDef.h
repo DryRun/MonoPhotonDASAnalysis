ea9ed3d2ada3440d668211f96599da95f70452f4
DryRun <david.renhwa.yu@gmail.com>
2019-01-11 14:54:40 -0600

    Remove reference to deleted TTree::MakeSelector class

--- src/LinkDef.h
+++ src/LinkDef.h
@@ -1,10 +1,6 @@
 #include "MonoPhoton/DASAnalysis/interface/NtupleData.h"
 
 #ifdef __CINT__
-#pragma extra_include "MonoPhoton/DASAnalysis/interface/NtupleData.h";
-#endif
-
-#ifdef __CINT__
 
 #pragma link off all globals;
 #pragma link off all classes;
@@ -13,6 +9,3 @@
 
 #endif
 
-#ifdef __CINT__
-#pragma link C++ class NtupleData+;
-#endif
